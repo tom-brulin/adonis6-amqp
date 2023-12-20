26438fdab85ded7faf43ef0838f71d2c928d3f00
tom-brulin <t.brulin@outlook.com>
2023-12-20 14:05:27 +0100

    chore: export errors

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ export { defineConfig } from './src/define_config.js'
 export { QueueHandler } from './src/amqp/queue_handler.js'
 export type { AmqpQueues } from './src/amqp/amqp_queues.js'
 export { amqp } from './src/amqp/amqp.js'
+export * as errors from './src/errors.js'
 
 export { configure } from './configure.js'
 export { stubsRoot } from './stubs/main.js'
